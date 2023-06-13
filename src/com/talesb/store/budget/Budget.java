5f4b990d1b47ae1c02ab1138cbb28d1118899600
talesb <tales.batista@temasistemas.com.br>
2023-06-12 23:17:42 -0300

    First commit

---
+++ src/com/talesb/store/budget/Budget.java
@@ -0,0 +1,19 @@
+package com.talesb.store.budget;
+
+import java.math.BigDecimal;
+
+public class Budget {
+	
+	private BigDecimal value;
+	
+	public Budget(BigDecimal value) {
+		super();
+		this.value = value;
+	}
+
+	public BigDecimal getValue() {
+		return value;
+	}
+ 
+
+}
