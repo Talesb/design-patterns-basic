18e2947cd9893812348e12791b2877f143156f1f
talesb <tales.batista@temasistemas.com.br>
2023-06-15 00:36:47 -0300

    Adding dicount business logic with code smells

---
+++ src/com/talesb/store/DiscountTest.java
@@ -0,0 +1,28 @@
+package com.talesb.store;
+
+import java.math.BigDecimal;
+
+import com.talesb.store.budget.Budget;
+import com.talesb.store.discount.DiscountCalculator;
+
+public class DiscountTest {
+
+	
+	
+	
+	public static void main(String[] args) {
+		
+		Budget firstBudget = new Budget(new BigDecimal("200"),6);
+		Budget secondBudget = new Budget(new BigDecimal("1000"),4);
+		
+		
+		
+		DiscountCalculator calculator = new DiscountCalculator();
+		System.out.println(calculator.calculate(firstBudget));
+		System.out.println(calculator.calculate(secondBudget));
+		
+	}
+	
+	
+	
+}
--- src/com/talesb/store/budget/Budget.java
+++ src/com/talesb/store/budget/Budget.java
@@ -5,7 +5,24 @@ import java.math.BigDecimal;
 public class Budget {
 	
 	private BigDecimal value;
+	private int itemQuantity;
 	
+	
+	
+	public Budget(BigDecimal value, int itemQuantity) {
+		super();
+		this.value = value;
+		this.itemQuantity = itemQuantity;
+	}
+
+	public int getItemQuantity() {
+		return itemQuantity;
+	}
+
+	public void setItemQuantity(int itemQuantity) {
+		this.itemQuantity = itemQuantity;
+	}
+
 	public Budget(BigDecimal value) {
 		super();
 		this.value = value;
