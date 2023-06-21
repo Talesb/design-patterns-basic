42699ceda757e8bec19f027974f0f56893e2a688
talesb <tales.batista@temasistemas.com.br>
2023-06-21 00:08:38 -0300

    Implementing budget situation with code smells

--- src/com/talesb/store/budget/Budget.java
+++ src/com/talesb/store/budget/Budget.java
@@ -3,12 +3,11 @@ package com.talesb.store.budget;
 import java.math.BigDecimal;
 
 public class Budget {
-	
+
 	private BigDecimal value;
 	private int itemQuantity;
-	
-	
-	
+	private String budgetSituation;
+
 	public Budget(BigDecimal value, int itemQuantity) {
 		super();
 		this.value = value;
@@ -31,6 +30,19 @@ public class Budget {
 	public BigDecimal getValue() {
 		return value;
 	}
- 
+
+	public void applyExtraDiscount() {
+		BigDecimal extraDiscountValue = BigDecimal.ZERO;
+		if (this.budgetSituation.equals("ANALYZING")) {
+			extraDiscountValue = new BigDecimal("0.05");
+		} else if (this.budgetSituation.equals("APPROVED")) {
+			extraDiscountValue = new BigDecimal("0.02");
+		}
+		this.value = this.value.subtract(this.value.multiply(extraDiscountValue));
+	}
+
+	public void aprove() {
+		this.budgetSituation = "APPROVED";
+	}
 
 }
