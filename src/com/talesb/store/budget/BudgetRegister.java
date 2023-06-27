953ad9ae90f61a324c128650e455cf0fc611e103
talesb <tales.batista@temasistemas.com.br>
2023-06-27 00:11:49 -0300

    Adding fake external api call rule with code smells

---
+++ src/com/talesb/store/budget/BudgetRegister.java
@@ -0,0 +1,16 @@
+package com.talesb.store.budget;
+
+public class BudgetRegister {
+
+	public void register(Budget budget) {
+
+		//Http call step can be:
+		//Pure java URL Connection or..
+		//Apache HTTP client libs or..
+		//Rest client libs etc etc.
+		
+		
+		
+	}
+
+}
